eaac792f2c54c6fdd739187ecfd94fd5a26f619f
arjunmathewdan <arjunmathewdan@gmail.com>
2017-09-05 01:43:35 -0400

    _start inline assembly code added (initial attempt, not perfect)

--- crt/crt1.c
+++ crt/crt1.c
@@ -2,4 +2,18 @@
 
 void _start(void) {
   // call main() and exit() here
+  __asm__(
+    "xorl %ebp, %ebp;"
+    "movq %rdx, %r9;"
+    "popq %rsi;"
+    "movq %rsp, %rdx;"
+    "andq $~15, %rsp;"
+    "pushq %rax;"
+    "pushq %rsp;"
+    "movq (main), %rdi;"
+    "call main;"
+    "movl $1, %eax;"
+    "xorl %ebx, %ebx;"
+    "int $0x80"
+  );
 }
